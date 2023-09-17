77ce3259b01b03f9a057653784a06c9a0009d3c6
Jeffrey Liu <jeffreyliu97@gmail.com>
2023-09-16 21:12:01 -0700

    api working and creating components

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,5 +1,25 @@
+<script setup>
+import streamCards from "./streamCards.vue";
+import { fetchTopStreams } from "../apiUtils.mjs";
+import { onMounted, ref, computed } from "vue";
+
+const topStreamsData = ref([]);
+
+onMounted(async () => {
+	try {
+		const response = await fetchTopStreams();
+		topStreamsData.value = response.data;
+	} catch (error) {
+		console.error("Error fetching data:", error);
+	}
+});
+
+const topStreamsDataComputed = computed(() => topStreamsData.value);
+</script>
+
 <template>
-  <div>
-    <h1>Dashboard here yay!</h1>
-  </div>
+	<div>
+		<h1>Dashboard here yay!</h1>
+		<streamCards :data="topStreamsDataComputed" />
+	</div>
 </template>
---
+++ src/components/streamCards.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+	props: {
+		data: Object,
+	},
+	mounted() {
+		// Log the data object to the console
+		// console.log(this.data);
+	},
+	methods: {
+		generateThumbnailURL(url) {
+			const width = 800;
+			const height = 400;
+			return url.replace("{width}", width).replace("{height}", height);
+		},
+	},
+};
+</script>
+
+<template>
+	<div>
+		<h1>Top Streamers Currently Live</h1>
+		<div v-for="(stream, index) in data" :key="index" class="stream-card">
+			<h2>{{ stream.user_name }}</h2>
+			<h3>{{ stream.title }}</h3>
+			<img :src="generateThumbnailURL(stream.thumbnail_url)" />
+			<p>Viewers: {{ stream.viewer_count }}</p>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.stream-card {
+	color: rgb(37, 37, 37);
+	border-radius: 9px;
+	border: 1px solid red;
+	padding: 5px;
+}
+</style>
