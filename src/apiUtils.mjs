2b9e948a6061d696790fb7b7ba45417d4d86473a
Jeff L <jeffreyliu97@gmail.com>
2023-09-18 15:36:48 -0700

    started new twitch api endpoint for user specific video data

--- src/apiUtils.mjs
+++ src/apiUtils.mjs
@@ -6,31 +6,46 @@ import { secretKey } from "./config";
 const getTopStreamsURL = "https://api.twitch.tv/helix/streams";
 
 export const fetchTopStreams = async () => {
-	try {
-		const response = await axios.get(getTopStreamsURL, {
-			headers: {
-				"Client-Id": clientId,
-				Authorization: secretKey,
-			},
-		});
-		return response.data;
-	} catch (error) {
-		throw new Error("Error fetching from API");
-	}
+  try {
+    const response = await axios.get(getTopStreamsURL, {
+      headers: {
+        "Client-Id": clientId,
+        Authorization: secretKey,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error("Error fetching from API");
+  }
 };
 
 const getTopGamesURL = "https://api.twitch.tv/helix/games/top";
 
 export const fetchTopGames = async () => {
-	try {
-		const response = await axios.get(getTopGamesURL, {
-			headers: {
-				"Client-Id": clientId,
-				Authorization: secretKey,
-			},
-		});
-		return response.data;
-	} catch (error) {
-		throw new Error("Error fetching from API");
-	}
+  try {
+    const response = await axios.get(getTopGamesURL, {
+      headers: {
+        "Client-Id": clientId,
+        Authorization: secretKey,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error("Error fetching from API");
+  }
+};
+
+export const fetchUserVideos = async (user_id) => {
+  const url = `https://api.twitch.tv/helix/videos?user_id=${user_id}`;
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        "Client-Id": clientId,
+        Authorization: secretKey,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error("Error fetching from API");
+  }
 };
--- src/components/streamDetails.vue
+++ src/components/streamDetails.vue
@@ -1,8 +1,45 @@
 <script>
-export default {};
+import { onMounted, ref, computed } from "vue";
+import { fetchUserVideos } from "../apiUtils.mjs";
+import { useRouter, useRoute } from "vue-router";
+
+export default {
+  setup() {
+    const topUserVidData = ref([]);
+    const router = useRouter();
+    const route = useRoute();
+
+    onMounted(async () => {
+      try {
+        const response = await fetchUserVideos(route.params.user_id);
+        topUserVidData.value = response.data;
+        console.log(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    });
+
+    const topUserVidDataComputed = computed(() => topUserVidData.value);
+
+    return {
+      topUserVidDataComputed,
+      router,
+      route,
+    };
+  },
+};
 </script>
 
 <template>
-  <div>Stream details go here</div>
-  <p>User ID: {{ $route.params.user_id }}</p>
+  <div>
+    <div>Stream details go here</div>
+    <p>User ID: {{ route.params.user_id }}</p>
+    <div v-for="(video, index) in topUserVidDataComputed" :key="index">
+      <div>
+        <h3>{{ video.title }}</h3>
+        <h3>{{ video.user_name }}</h3>
+        <h3>{{ video.thumbnail_url }}</h3>
+      </div>
+    </div>
+  </div>
 </template>
